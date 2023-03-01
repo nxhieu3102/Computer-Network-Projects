d557e6f4200352b3afd57b03ccc199f862861455
elliotnguyen <nguyentienthanhgq@gmail.com>
2023-03-01 14:24:52 +0700

    Web Browser

---
+++ demo/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '92f16fef8144ffe1be36103dd5ca2730'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from demo import routes
+    
