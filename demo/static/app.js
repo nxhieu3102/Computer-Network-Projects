3de2e2a2c8040d63c8fe2ffcb42d76868f0ae302
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-03-09 14:32:01 +0700

    fix overlay bug

--- demo/static/app.js
+++ demo/static/app.js
@@ -2,9 +2,9 @@ class Chatbox {
     constructor() {
         this.args = {
             openButton: document.querySelector('.chatbox__button'),
-            chatBox : document.querySelector('.chatbox__support'),
-            sendButton : document.querySelector('.send__button'),
-            overlay : document.getElementById('overlay')
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button'),
+            overlay: document.getElementById('overlay')
         }
 
         this.state = false;
@@ -16,28 +16,28 @@ class Chatbox {
         // The speech recognition interface lives on the browser’s window object
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
 
-        if(SpeechRecognition) {
+        if (SpeechRecognition) {
             console.log("Your Browser supports speech Recognition");
-            
+
             const recognition = new SpeechRecognition();
             recognition.continuous = true;
             // recognition.lang = "en-US";
-        
+
             searchForm.insertAdjacentHTML("beforeend", '<button type="button"><i class="fas fa-microphone"></i></button>');
-        
+
             const micBtn = searchForm.querySelector("button");
             const micIcon = micBtn.firstElementChild;
-        
+
             micBtn.addEventListener("click", micBtnClick);
             function micBtnClick() {
-                if(micIcon.classList.contains("fa-microphone")) { // Start Voice Recognition
+                if (micIcon.classList.contains("fa-microphone")) { // Start Voice Recognition
                     recognition.start(); // First time you have to allow access to mic!
                 }
                 else {
                     recognition.stop();
                 }
             }
-        
+
             recognition.addEventListener("start", startSpeechRecognition); // <=> recognition.onstart = function() {...}
             function startSpeechRecognition() {
                 micIcon.classList.remove("fa-microphone");
@@ -45,7 +45,7 @@ class Chatbox {
                 searchFormInput.focus();
                 //console.log("Voice activated, SPEAK");
             }
-        
+
             recognition.addEventListener("end", endSpeechRecognition); // <=> recognition.onend = function() {...}
             function endSpeechRecognition() {
                 micIcon.classList.remove("fa-microphone-slash");
@@ -53,20 +53,20 @@ class Chatbox {
                 searchFormInput.focus();
                 //console.log("Speech recognition service disconnected");
             }
-        
+
             recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
             function resultOfSpeechRecognition(event) {
                 const current = event.resultIndex;
                 const transcript = event.results[current][0].transcript;
-                
-                if(transcript.toLowerCase().trim()==="stop recording") {
+
+                if (transcript.toLowerCase().trim() === "stop recording") {
                     recognition.stop();
                 }
-                else if(!searchFormInput.value) {
+                else if (!searchFormInput.value) {
                     searchFormInput.value = transcript;
-                } else if(transcript.toLowerCase().trim()==="go") {
+                } else if (transcript.toLowerCase().trim() === "go") {
                     searchForm.submit();
-                } else if(transcript.toLowerCase().trim()==="reset input") {
+                } else if (transcript.toLowerCase().trim() === "reset input") {
                     searchFormInput.value = "";
                 } else {
                     searchFormInput.value = transcript;
@@ -79,21 +79,30 @@ class Chatbox {
         }
     }
 
+    closeModal(chatbox) {
+        if (chatbox == null) return;
+        chatbox.classList.remove('chatbox--active');
+        /*const overlay = document.getElementById('overlay');*/
+        overlay.classList.remove('active');
+        this.state = !this.state;
+
+    }
+
     display() {
-        const {openButton, chatBox, sendButton, overlay} = this.args;
-        // console.log("hello");
+        const _this = this
+        const { openButton, chatBox, sendButton, overlay } = this.args;
         overlay.addEventListener('click', () => {
-            const temps = chatBox.querySelectorAll('.chatbox--active')
+            const temps = document.querySelectorAll('.chatbox--active')
             temps.forEach(temp => {
-                closeModal(temp)
-              })
+                _this.closeModal(temp)
+            })
         })
 
         openButton.addEventListener('click', () => this.toggleState(chatBox))
-        sendButton.addEventListener('click',() => this.onSendButton(chatBox))
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox))
 
         const node = chatBox.querySelector('input');
-        node.addEventListener("keypress",({key : string}) => {
+        node.addEventListener("keypress", ({ key: string }) => {
             if (string === "Enter") {
                 this.onSendButton(chatBox);
             }
@@ -110,13 +119,7 @@ class Chatbox {
         //openButton.classList.add('active');
     }
 
-    closeModal(chatbox) {
-        if (chatbox == null) return;
-        chatbox.classList.remove('chatbox--active');
-        /*const overlay = document.getElementById('overlay');*/
-        overlay.classList.remove('active');
 
-    }
 
     toggleState(chatbox) {
         this.state = !this.state;
@@ -132,56 +135,54 @@ class Chatbox {
         }
     }
 
-    onSendButton(chatbox) {
-        var textField = chatbox.querySelector('input');
-        let text1 = textField.value
-        if (text1 === "") {
+    async onSendButton(chatbox) {
+        const textField = chatbox.querySelector('input');
+        const text1 = textField.value
+        if (text1 === "")
             return;
-        }
 
-        let msg1 = {name : "User", message : text1, type : "text"}
+        let msg1 = { name: "User", message: text1, type: "text" }
         console.log(msg1.message);
         this.messages.push(msg1);
         this.updateChatText(chatbox);
         textField.value = '';
 
-        fetch($SCRIPT_ROOT + '/predict',{
+        await fetch($SCRIPT_ROOT + '/predict', {
             method: 'POST',
-            body : JSON.stringify({message : text1}),
-            mode : 'cors',
-            headers : {
-                'Content-Type' : 'application/json'
+            body: JSON.stringify({ message: text1 }),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
             },
-        })
-        .then(r => r.json())
-        .then(r => {
-            let msg2 = {name : "GPT", message : r.answer, type : r.type}
-            this.messages.push(msg2);
-            this.updateChatText(chatbox);
-            textField.value = '';
-        }).catch((error) => {
-            console.error('Error:' , error);
-            this.updateChatText(chatbox);
-            textField.value = '';
-        });
+        }).then(res => res.json())
+            .then(res => {
+                let msg2 = { name: "GPT", message: res.answer, type: res.type }
+                this.messages.push(msg2);
+                this.updateChatText(chatbox);
+                textField.value = '';
+            }).catch((error) => {
+                console.error('Error:', error);
+                this.updateChatText(chatbox);
+                textField.value = '';
+            });
     }
 
     updateChatText(chatbox) {
         const html = chatbox.querySelector('.chatbox__messages');
         html.innerHTML = '';
-        this.messages.slice().reverse().forEach(function(item,index) {
+        this.messages.slice().reverse().forEach(function (item, index) {
             if (item.name === "GPT") {
-               if (item.type === "image") {
+                if (item.type === "image") {
                     const imageElement = document.createElement('img');
                     imageElement.src = item.message;
                     imageElement.className = "messages__item messages__item--visitor";
                     html.appendChild(imageElement);
-               } else if (item.type === "text") {
+                } else if (item.type === "text") {
                     const textElement = document.createElement('div');
                     textElement.className = "messages__item messages__item--visitor";
                     textElement.textContent = item.message;
                     html.appendChild(textElement);
-               }
+                }
             } else {
                 const textElement = document.createElement('div');
                 textElement.className = "messages__item messages__item--operator";
