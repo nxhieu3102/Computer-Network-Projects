3fe3981a96213c98a0ed18207e97f6c40f585ac4
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-03-09 20:46:20 +0700

    fix bug

--- demo/static/voice_record.js
+++ demo/static/voice_record.js
@@ -10,7 +10,7 @@ const micIcon = recordBtn.firstElementChild;
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({
         audio: true
-    }).then( async (stream) => {
+    }).then(async (stream) => {
         mediaRecorder = new MediaRecorder(stream)
 
         mediaRecorder.ondataavailable = (e) => {
@@ -18,21 +18,35 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         }
 
         mediaRecorder.onstop = async () => {
-            const download = () =>{
-                const blob = new Blob(chunks, { 'type': 'audio/mp3; codecs=opus' })
-                chunks = []
-                audioURL = window.URL.createObjectURL(blob)
-                const downloadLink = document.createElement('a')
-                downloadLink.href = audioURL
-                audioName = 'audio' + Math.floor(Math.random() * 1000);
-                console.log(audioName);
-                downloadLink.setAttribute('download', audioName)
-                downloadLink.click()
-            }
+            const blob = new Blob(chunks, { 'type': 'audio/mp3; codecs=opus' })
+            chunks = []
+            audioURL = window.URL.createObjectURL(blob)
+            const downloadLink = document.createElement('a')
+            downloadLink.href = audioURL
+            audioName = 'audio' + Math.floor(Math.random() * 1000);
 
-            download()
+            downloadLink.setAttribute('download', audioName)
+            downloadLink.click()
+
+            setTimeout( async() => {
+                console.log("download complete");
+                await fetch($SCRIPT_ROOT + '/whisper', {
+                    method: 'POST',
+                    body: JSON.stringify({ name: audioName + ".mp3" }),
+                    mode: 'cors',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                }).then(res => res.json())
+                    .then(res => {
+                        let message = res.transcript.text
+                        inputText.value = message;
+                    }).catch((error) => {
+                        console.error('Error:', error);
+                        inputText.value = '';
+                    });
+            }, 5000)
         }
-
     }).catch(error => {
         console.log('Following error has occured : ', error)
     })
@@ -46,38 +60,17 @@ const startRecord = () => {
 }
 
 const stopRecord = async () => {
-    await mediaRecorder.stop()
+    mediaRecorder.stop()
 }
-
 
 recordBtn.addEventListener('click', async () => {
     if (micIcon.classList.contains("fa-microphone")) {
         micIcon.classList.remove("fa-microphone");
         micIcon.classList.add("fa-microphone-slash");
-
         startRecord()
     } else {
         micIcon.classList.add("fa-microphone");
         micIcon.classList.remove("fa-microphone-slash");
-
-        await stopRecord()
-
-        console.log(audioName)
-
-        await fetch($SCRIPT_ROOT + '/whisper', {
-            method: 'POST',
-            body: JSON.stringify({ name: audioName }),
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(res => res.json())
-            .then(res => {
-                let message = res.transcript
-                inputText.value = message;
-            }).catch((error) => {
-                console.error('Error:', error);
-                inputText.value = '';
-            });
+        stopRecord()
     }
 })
